311b47c798a3f5ec251e5f53a6739d294695c081
Greg Roberts <groberts3104@gmail.com>
2020-03-14 18:26:59 -0400

    removed extra link for About

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -6,9 +6,6 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        About
-      </Link>
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
