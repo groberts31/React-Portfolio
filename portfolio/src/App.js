a7f5c2e6770ed4244b4c2cde9e8981e8dce567a4
Greg Roberts <groberts3104@gmail.com>
2020-03-14 16:53:38 -0400

    added navbar, wrapper and footer components and added index.css and app.css. Also imported components and pages to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,26 @@
-import React, { Component } from "react";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import Portfolio from "./components/Portfolio";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
+export default App;
