75b843d897d8999b4bd967bc52d5cd5a1948e08a
Greg Roberts <groberts3104@gmail.com>
2020-03-15 23:13:12 -0400

    added info to footer component

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Portfolio</span>
+      <span>Greg Roberts Portfolio</span>
     </footer>
   );
 }
