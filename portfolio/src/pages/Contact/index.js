27ca392bbc7fe464cca29344ac3478cefe4c76cd
Greg Roberts <groberts3104@gmail.com>
2020-03-15 21:47:40 -0400

    removed container and row component from Contact page

--- portfolio/src/pages/Contact/index.js
+++ portfolio/src/pages/Contact/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./style.css";
-import Container from "../../components/Container";
-import Row from "../../components/Row";
 import Form from "../../components/Form";
 
 function Contact() {
   return (
     <div className="entireArea">
       <div className="jumbotron">
-        <Container>
-          <Row>
             <Form />
-          </Row>
-        </Container>
       </div>
     </div>
   );
