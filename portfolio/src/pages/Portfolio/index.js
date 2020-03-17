38eb939140dc8dd17c1889328c84580cc1a08879
Greg Roberts <groberts3104@gmail.com>
2020-03-17 13:45:03 -0400

    added styling to page and added Col compnent back to components file

--- portfolio/src/pages/Portfolio/index.js
+++ portfolio/src/pages/Portfolio/index.js
@@ -6,6 +6,7 @@ import Links from "../../components/Links";
 
 function Portfolio() {
   return (
+      <div className="background">
     <div className="jumbotron">
       <div className="portInfo">
         <p>Portfolio</p>
@@ -15,34 +16,35 @@ function Portfolio() {
       <div className="linebreak">
         <hr size="1" color="black"></hr>
       </div>
-      <PortInfo
-        image={info[0].image}
-        name={info[0].name}
-        projectName={info[0].projectName}
-        webpage={info[0].webpage}
-        githubRepo={info[0].githubRepo}
-      />
-      <PortInfo
-        image={info[1].image}
-        name={info[1].name}
-        projectName={info[1].projectName}
-        webpage={info[1].webpage}
-        githubRepo={info[1].githubRepo}
-      />
-      <PortInfo
-        image={info[2].image}
-        name={info[2].name}
-        projectName={info[2].projectName}
-        webpage={info[2].webpage}
-        githubRepo={info[2].githubRepo}
-      />
-      <PortInfo
-        image={info[3].image}
-        name={info[3].name}
-        projectName={info[3].projectName}
-        webpage={info[3].webpage}
-        githubRepo={info[3].githubRepo}
-      />
+        <PortInfo
+          image={info[0].image}
+          name={info[0].name}
+          projectName={info[0].projectName}
+          webpage={info[0].webpage}
+          githubRepo={info[0].githubRepo}
+        />
+        <PortInfo
+          image={info[1].image}
+          name={info[1].name}
+          projectName={info[1].projectName}
+          webpage={info[1].webpage}
+          githubRepo={info[1].githubRepo}
+        />
+        <PortInfo
+          image={info[2].image}
+          name={info[2].name}
+          projectName={info[2].projectName}
+          webpage={info[2].webpage}
+          githubRepo={info[2].githubRepo}
+        />
+        <PortInfo
+          image={info[3].image}
+          name={info[3].name}
+          projectName={info[3].projectName}
+          webpage={info[3].webpage}
+          githubRepo={info[3].githubRepo}
+        />
+      </div>
     </div>
   );
 }
