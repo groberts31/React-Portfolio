3a2edc026b8a174095e61a915cd645910348f6c0
Greg Roberts <groberts3104@gmail.com>
2020-03-17 13:50:01 -0400

    added background images to each page

--- portfolio/src/pages/Contact/index.js
+++ portfolio/src/pages/Contact/index.js
@@ -5,17 +5,19 @@ import Links from "../../components/Links";
 
 function Contact() {
   return (
-    <div className="jumbotron">
-      <span className="contactMe">
-        <p>Contact Me</p>
-      </span>
-      <Links />
-      <div className="clearfix"></div>
-      <div className="linebreak">
-        <hr size="1" color="black"></hr>
+    <div className="background">
+      <div className="jumbotron">
+        <span className="contactMe">
+          <p>Contact Me</p>
+        </span>
+        <Links />
+        <div className="clearfix"></div>
+        <div className="linebreak">
+          <hr size="1" color="black"></hr>
+        </div>
+        <div className="clearfix"></div>
+        <Form />
       </div>
-      <div className="clearfix"></div>
-      <Form />
     </div>
   );
 }
--- portfolio/src/pages/Resume/index.js
+++ portfolio/src/pages/Resume/index.js
@@ -4,16 +4,17 @@ import Links from "../../components/Links";
 
 function Resume() {
   return (
-    <div className="jumbotron">
-      <span className="resume">
-        <p>My Resume</p>
-      </span>
-      <Links />
-      <div className="clearfix"></div>
-      <div className="linebreak">
-        <hr size="1" color="grey"></hr>
-      </div>
-      <div className="Container">
+    <div className="background">
+      <div className="jumbotron">
+        <span className="resume">
+          <p>My Resume</p>
+        </span>
+        <Links />
+        <div className="clearfix"></div>
+        <div className="linebreak">
+          <hr size="1" color="grey"></hr>
+        </div>
+        <div className="Container"></div>
       </div>
     </div>
   );
