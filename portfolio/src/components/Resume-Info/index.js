60c575fc3ed089df5345d699082c6f2034314920
Greg Roberts <groberts3104@gmail.com>
2020-11-10 21:33:49 -0500

    added pic to Resume Page to test pic before attempting PDF

---
+++ portfolio/src/components/Resume-Info/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Container from "../Container";
+import Row from "../Row";
+import "./style.css";
+
+function ResInfo(props) {
+  return (
+    <Container>
+      <div className="fullArea">
+        <Row>
+          <div className="picCol">
+            <div className="container">
+              <div className="img-container" size="50%">
+                <img alt={props.name} src={props.image} />
+              </div>
+            </div>
+          </div>
+          <div className="projData">
+            <div className="container">
+              <h3>{props.projectName}</h3>
+              <br />
+              <h5>Webpage:</h5>
+              <p>{props.webpage}</p>
+              <h5>GitHub Repo:</h5>
+              <p>{props.githubRepo}</p>
+            </div>
+          </div>
+        </Row>
+      </div>
+    </Container>
+  );
+}
+
+export default ResInfo;
--- portfolio/src/pages/Resume/index.js
+++ portfolio/src/pages/Resume/index.js
@@ -1,22 +1,32 @@
 import React from "react";
 import "./style.css";
+import ResInfo from "../../components/Resume-Info";
+import info from "../../portfolio.json";
 import Links from "../../components/Links";
+import Container from "../../components/Container";
 
 function Resume() {
   return (
     <div className="background">
       <div className="jumbotron">
-        <span className="resume">
+        <div className="portInfo">
           <p>My Resume</p>
-        </span>
+        </div>
         <Links />
         <div className="clearfix"></div>
         <div className="linebreak">
-          <hr size="1" color="grey"></hr>
+          <hr size="1" color="black"></hr>
         </div>
-        <div className="Container">
-          <h1>Hi there!!! Im Greg...</h1>
-        </div>
+        <Container>
+          <div className="image">
+            <img
+              src="../../../images/image2.jpeg"
+              className="img-fluid"
+              height="auto"
+              alt="Greg"
+            />
+          </div>
+        </Container>
       </div>
     </div>
   );
